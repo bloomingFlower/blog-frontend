b9781b41a6b237514c2580771420016d390fe9e7
AAII <sinsunby@gmail.com>
2024-07-14 17:41:36 +0900

    trigger

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,24 +4,24 @@ import backgroundImage1 from "@img/404.png";
 import backgroundImage2 from "@img/404_2.png";
 
 function NotFound() {
-    const images = [backgroundImage1, backgroundImage2];
-    const randomImage = images[Math.floor(Math.random() * images.length)];
+  const images = [backgroundImage1, backgroundImage2];
+  const randomImage = images[Math.floor(Math.random() * images.length)];
 
-    return (
-        <div
-            className="flex flex-col items-center justify-center h-screen text-center"
-            style={{
-                backgroundImage: `url(${randomImage})`,
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-            }}
-        >
-            {/*<h1 className="text-6xl text-red-600">404</h1>*/}
-            {/*<p className="text-2xl text-gray-700">*/}
-            {/*  Oops! The page you are looking for does not exist.*/}
-            {/*</p>*/}
-        </div>
-    );
+  return (
+    <div
+      className="flex flex-col items-center justify-center h-screen text-center"
+      style={{
+        backgroundImage: `url(${randomImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      {/*<h1 className="text-6xl text-red-600">404</h1>*/}
+      {/*<p className="text-2xl text-gray-700">*/}
+      {/*  Oops! The page you are looking for does not exist.*/}
+      {/*</p>*/}
+    </div>
+  );
 }
 
 export default NotFound;
