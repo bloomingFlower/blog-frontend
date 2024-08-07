611492d6ea360d416abb7a98d295a48e462d3a82
jy form air <sinsunby@gmail.com>
2024-08-07 18:05:45 +0900

    loading indicator

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 import LoadingIndicator from "./pages/components/LoadingIndicator";
 import { AuthProvider, AuthContext } from "./pages/components/AuthContext";
 import { api } from "./pages/components/api";
+import backgroundImage from "@img/background2.webp";
 
 import "tailwindcss/tailwind.css";
 import "./styles/loading.css";
@@ -94,114 +95,116 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className="flex flex-col min-h-screen">
-          <nav className="fixed top-0 left-0 right-0 flex items-center justify-between p-2 sm:p-3 bg-white text-black h-[50px] sm:h-[60px] font-serif z-50">
-            <Link to="/" className="text-lg sm:text-xl">
-              Our Journey
-            </Link>
-            <div className="flex items-center" ref={searchRef}>
-              <AuthContext.Consumer>
-                {({ isLoggedIn }) =>
-                  isLoggedIn && (
-                    <span className="hidden md:inline-block mr-4 text-xs sm:text-sm">
-                      Remaining login time: {formatTime(remainingTime)}
-                    </span>
-                  )
-                }
-              </AuthContext.Consumer>
-              <div className="relative mr-2 sm:mr-4">
-                <button
-                  onClick={toggleSearchInput}
-                  className="p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-5000"
-                  aria-label="Search"
-                >
-                  <svg
-                    className="h-5 w-5 sm:h-6 sm:w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+        <div className="min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${backgroundImage})` }}>
+          <div className="flex flex-col min-h-screen">
+            <nav className="fixed top-0 left-0 right-0 flex items-center justify-between p-2 sm:p-3 bg-white text-black h-[50px] sm:h-[60px] font-serif z-50">
+              <Link to="/" className="text-lg sm:text-xl">
+                Our Journey
+              </Link>
+              <div className="flex items-center" ref={searchRef}>
+                <AuthContext.Consumer>
+                  {({ isLoggedIn }) =>
+                    isLoggedIn && (
+                      <span className="hidden md:inline-block mr-4 text-xs sm:text-sm">
+                        Remaining login time: {formatTime(remainingTime)}
+                      </span>
+                    )
+                  }
+                </AuthContext.Consumer>
+                <div className="relative mr-2 sm:mr-4">
+                  <button
+                    onClick={toggleSearchInput}
+                    className="p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-5000"
+                    aria-label="Search"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    />
-                  </svg>
-                </button>
-                {isSearchInputVisible && (
-                  <form
-                    onSubmit={handleSearch}
-                    className="absolute right-0 mt-2 w-64"
-                  >
-                    <input
-                      ref={searchInputRef}
-                      type="text"
-                      value={searchQuery}
-                      onChange={(e) => setSearchQuery(e.target.value)}
-                      placeholder="ElasticSearch not yet implemented..."
-                      className="w-full px-3 py-1 sm:px-4 sm:py-2 text-sm border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                    />
-                  </form>
-                )}
-              </div>
-              <Suspense fallback={<LoadingIndicator />}>
-                <HamburgerButton />
-              </Suspense>
-            </div>
-          </nav>
-          <main className="flex-grow mt-[50px] sm:mt-[60px]">
-            {isSearchOpen && (
-              <div className="w-full md:w-2/3 bg-white shadow-md z-50">
+                    <svg
+                      className="h-5 w-5 sm:h-6 sm:w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                      />
+                    </svg>
+                  </button>
+                  {isSearchInputVisible && (
+                    <form
+                      onSubmit={handleSearch}
+                      className="absolute right-0 mt-2 w-64"
+                    >
+                      <input
+                        ref={searchInputRef}
+                        type="text"
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        placeholder="ElasticSearch not yet implemented..."
+                        className="w-full px-3 py-1 sm:px-4 sm:py-2 text-sm border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                      />
+                    </form>
+                  )}
+                </div>
                 <Suspense fallback={<LoadingIndicator />}>
-                  <SearchResults
-                    results={searchResults}
-                    onClose={() => setIsSearchOpen(false)}
-                  />
+                  <HamburgerButton />
                 </Suspense>
               </div>
-            )}
-            <Suspense fallback={<LoadingIndicator />}>
-              <ActivityMonitor onTimeUpdate={setRemainingTime} />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/post" element={<Post />} />
-                <Route path="/post/upload" element={<PostUpload />} />
-                <Route path="/scrap" element={<Scrap />} />
-                <Route path="/rust-news" element={<RustNews />} />
-                <Route path="/bitcoin-price" element={<BitcoinPricePage />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/admin-login" element={<AdminLogin />} />
-                <Route path="/logout" element={<Logout />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/edit-profile" element={<EditProfile />} />
-                <Route path="/system-stack" element={<SystemStack />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </Suspense>
-          </main>
-          <ToastContainer
-            position="top-center"
-            autoClose={1000}
-            hideProgressBar={false}
-            newestOnTop={true}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-            className="sm:text-sm md:text-base"
-            toastClassName={() =>
-              "relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer bg-white bg-clip-padding border border-gray-200 shadow-lg"
-            }
-            bodyClassName={() => "flex text-black font-medium block p-3"}
-          />
-          <footer className="fixed bottom-0 left-0 right-0 bg-white">
-            <Suspense fallback={<LoadingIndicator />}>
-              <Footer />
-            </Suspense>
-          </footer>
+            </nav>
+            <main className="flex-grow mt-[50px] sm:mt-[60px]">
+              {isSearchOpen && (
+                <div className="w-full md:w-2/3 bg-white shadow-md z-50">
+                  <Suspense fallback={<LoadingIndicator />}>
+                    <SearchResults
+                      results={searchResults}
+                      onClose={() => setIsSearchOpen(false)}
+                    />
+                  </Suspense>
+                </div>
+              )}
+              <Suspense fallback={<LoadingIndicator />}>
+                <ActivityMonitor onTimeUpdate={setRemainingTime} />
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/post" element={<Post />} />
+                  <Route path="/post/upload" element={<PostUpload />} />
+                  <Route path="/scrap" element={<Scrap />} />
+                  <Route path="/rust-news" element={<RustNews />} />
+                  <Route path="/bitcoin-price" element={<BitcoinPricePage />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/admin-login" element={<AdminLogin />} />
+                  <Route path="/logout" element={<Logout />} />
+                  <Route path="/signup" element={<Signup />} />
+                  <Route path="/edit-profile" element={<EditProfile />} />
+                  <Route path="/system-stack" element={<SystemStack />} />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </Suspense>
+            </main>
+            <ToastContainer
+              position="top-center"
+              autoClose={1000}
+              hideProgressBar={false}
+              newestOnTop={true}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+              className="sm:text-sm md:text-base"
+              toastClassName={() =>
+                "relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer bg-white bg-clip-padding border border-gray-200 shadow-lg"
+              }
+              bodyClassName={() => "flex text-black font-medium block p-3"}
+            />
+            <footer className="fixed bottom-0 left-0 right-0 bg-white">
+              <Suspense fallback={<LoadingIndicator />}>
+                <Footer />
+              </Suspense>
+            </footer>
+          </div>
         </div>
       </Router>
     </AuthProvider>
--- src/pages/components/LoadingIndicator.jsx
+++ src/pages/components/LoadingIndicator.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const LoadingIndicator = () => {
+    // State to control visibility of the loading indicator
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        // Set a timeout to show the loading indicator after 0.5 seconds
+        const timer = setTimeout(() => {
+            setIsVisible(true);
+        }, 500);
+
+        // Clean up the timer when the component unmounts
+        return () => clearTimeout(timer);
+    }, []); // Empty dependency array means this effect runs once on mount
+
+    // If not visible, render nothing
+    if (!isVisible) return null;
+
     return (
         <div className="animate-spin rounded-full h-16 w-16 sm:h-24 sm:w-24 md:h-32 md:w-32 lg:h-40 lg:w-40 border-t-2 border-b-2 border-white"></div>
     );
